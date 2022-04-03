67a721b37efb6c6d65552da431ab8fbc5a3ce351
drag13 <vitaliy.ruban@itera.no>
2022-04-03 13:39:01 +0300

    Added possiiblity to get links to the current slide

--- presentations/shared/init.js
+++ presentations/shared/init.js
@@ -3,8 +3,8 @@ import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 export const init = () => {
-  let deck = new Reveal({
+  new Reveal({
     plugins: [Markdown, Highlight],
-  });
-  deck.initialize();
+    hash: true,
+  }).initialize();
 };
