ccdb04206065ab7e3f7fa5621fa91fc2f8b94037
drag13 <vitaliy.ruban@itera.no>
2022-04-08 16:36:31 +0300

    Added code to handle long lists (better avoid this)

--- presentations/shared/init.js
+++ presentations/shared/init.js
@@ -3,8 +3,31 @@ import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 (function init() {
-  new Reveal({
+  const reveal = new Reveal({
     plugins: [Markdown, Highlight],
     hash: true,
-  }).initialize();
+  });
+
+  function resetSlideScrolling(slide) {
+    slide.classList.remove("scrollable-slide");
+  }
+
+  function handleSlideScrolling(slide) {
+    if (slide.scrollHeight >= 800) {
+      slide.classList.add("scrollable-slide");
+    }
+  }
+
+  reveal.addEventListener("ready", function (event) {
+    handleSlideScrolling(event.currentSlide);
+  });
+
+  reveal.addEventListener("slidechanged", function (event) {
+    if (event.previousSlide) {
+      resetSlideScrolling(event.previousSlide);
+    }
+    handleSlideScrolling(event.currentSlide);
+  });
+
+  reveal.initialize();
 })();
