f4d7d94f042a84be41a56be3ad1ae97ae68c23d3
drag13 <vitaliy.ruban@itera.no>
2022-04-27 16:06:26 +0300

    Fixed path error

--- tools/start.js
+++ tools/start.js
@@ -1,12 +1,12 @@
-const { exec } = require("child_process");
-const { join } = require("path");
-const course = require("../course.json");
+const { exec } = require('child_process');
+const { join } = require('path');
+const { cwd } = require('process');
+const course = require('../course.json');
 
 (function cli(params) {
   const [lessonNumber] = params;
   const lesson =
-    course.lessons[+lessonNumber] ??
-    course.lessons.find((x) => x.name === lessonNumber);
+    course.lessons[+lessonNumber] ?? course.lessons.find((x) => x.name === lessonNumber);
 
   if (!lesson) {
     console.log(`Lesson with number #${lessonNumber} doesn't exist, aborting`);
@@ -17,6 +17,6 @@ const course = require("../course.json");
 
   console.log(`Starting ${sub} presentation`);
 
-  const path = join(__dirname, "presentations", sub, "index.html");
+  const path = join(cwd(), 'presentations', sub, 'index.html');
   exec(`npx parcel ${path}`);
 })(process.argv.splice(2));
