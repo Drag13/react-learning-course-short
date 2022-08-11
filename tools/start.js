85956afa9b6bacce61f2ad829dc9f4206ce24e55
drag13 <someemail@fakedomain.bzz>
2022-08-11 13:45:24 +0300

    Improved start script to be more descriptive

--- tools/start.js
+++ tools/start.js
@@ -2,9 +2,18 @@ const { exec } = require('child_process');
 const { join } = require('path');
 const { cwd } = require('process');
 const course = require('../course.json');
+const PORT = process.env.PORT ?? '1234';
 
 (function cli(params) {
   const [lessonNumber] = params;
+
+  if (lessonNumber == undefined) {
+    console.log('Please, specify lesson name to start first');
+    console.log('Available lessons are: ');
+    course.lessons.map((l) => l.name).forEach((name) => console.log(name));
+    return;
+  }
+
   const lesson =
     course.lessons[+lessonNumber] ?? course.lessons.find((x) => x.name === lessonNumber);
 
@@ -18,5 +27,6 @@ const course = require('../course.json');
   console.log(`Starting ${sub} presentation`);
 
   const path = join(cwd(), 'presentations', sub, 'index.html');
+  console.log(`Starting developer server on http://localhost:${PORT}`);
   exec(`npx parcel ${path}`);
 })(process.argv.splice(2));
