fc0322be3d7185a0388240291b1511b68176e664
drag13 <someemail@fakedomain.bzz>
2022-08-18 17:26:45 +0300

    CLI improving

--- tools/start.js
+++ tools/start.js
@@ -8,18 +8,16 @@ const PORT = process.env.PORT ?? '1234';
   const [lessonNumber] = params;
 
   if (lessonNumber == undefined) {
-    console.log('Please, specify lesson name to start first');
-    console.log('Available lessons are: ');
-    course.lessons.map((l) => l.name).forEach((name) => console.log(name));
-    return;
+    console.log('\x1b[33m%s\x1b[0m', 'Please, specify lesson name to start first');
+    return logAvailableLessonNames(course.lessons);
   }
 
   const lesson =
     course.lessons[+lessonNumber] ?? course.lessons.find((x) => x.name === lessonNumber);
 
   if (!lesson) {
-    console.log(`Lesson with number #${lessonNumber} doesn't exist, aborting`);
-    return;
+    console.log('\x1b[33m%s\x1b[0m', `Lesson "${lessonNumber}" doesn't exist, aborting`);
+    return logAvailableLessonNames(course.lessons);
   }
 
   const sub = lesson.name;
@@ -29,4 +27,13 @@ const PORT = process.env.PORT ?? '1234';
   const path = join(cwd(), 'presentations', sub, 'index.html');
   console.log(`Starting developer server on http://localhost:${PORT}`);
   exec(`npx parcel ${path}`);
+
+  /**
+   * Logs all lesson names available to start
+   * @param {[{name:string}]} lessons
+   */
+  function logAvailableLessonNames(lessons) {
+    console.log('Available lessons are: ');
+    lessons.map((l) => l.name).forEach((name) => console.log(name));
+  }
 })(process.argv.splice(2));
