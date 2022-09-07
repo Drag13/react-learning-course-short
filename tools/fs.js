9ba84d3560c6f29b5df78809d9c6beca7e660573
drag13 <someemail@fakedomain.bzz>
2022-09-07 17:15:53 +0300

    Added code to mask the data

--- tools/fs.js
+++ tools/fs.js
@@ -1,14 +1,21 @@
-const { readFileSync, writeFileSync, existsSync } = require("fs");
+const { readFileSync, writeFileSync, existsSync } = require('fs');
 
-const writeFile = (path, data) =>
-  writeFileSync(path, data, { encoding: "utf-8" });
+const writeFile = (path, data) => writeFileSync(path, data, { encoding: 'utf-8' });
 
-const readFile = (path) => readFileSync(path, { encoding: "utf-8" });
+const readFile = (path) => readFileSync(path, { encoding: 'utf-8' });
 
 const exists = (path) => existsSync(path);
+
+const ensureExists = (path) => {
+  if (exists(path)) {
+    return;
+  }
+  throw new Error(`File: "${path}" not exists`);
+};
 
 module.exports = {
   writeFile,
   readFile,
   exists,
+  ensureExists,
 };
--- tools/mask.js
+++ tools/mask.js
@@ -1,5 +1,6 @@
-const { readFile, writeFile } = require('./fs');
+const { readFile, writeFile, ensureExists } = require('./fs');
 const { join } = require('path');
+const { cwd } = require('process');
 
 (function main(params) {
   function simplePipe(arg, ...args) {
@@ -10,6 +11,10 @@ const { join } = require('path');
     return line.split(delimiter)[1];
   }
 
+  function extractTg(line) {
+    return line;
+  }
+
   function normalizeName(name) {
     return name.replace(/"/g, '').toLowerCase();
   }
@@ -18,15 +23,31 @@ const { join } = require('path');
     return `***${name.substring(2)}`;
   }
 
-  const pathToFileWithNames = join(__dirname, params[0] ?? `../sample.csv`);
-  const pathToOutputFile = join(__dirname, params[0] ?? `../result.txt`);
+  function extractNamesFromGoogleForm(fileWithNames) {
+    return fileWithNames
+      .split('\n')
+      .slice(1)
+      .map((n) => simplePipe(n, extractName, normalizeName, mask));
+  }
+
+  function extractNamesPlainList(fileWithNames) {
+    return fileWithNames.map((n) => simplePipe(n, extractTg, normalizeName, mask));
+  }
+
+  const pathToFileWithNames = join(cwd(), params[0] ?? `sample.csv`);
+  const pathToOutputFile = join(cwd(), params[1] ?? `result.txt`);
+
+  ensureExists(pathToFileWithNames);
+  ensureExists(pathToOutputFile);
 
   const fileWithNames = readFile(pathToFileWithNames);
-  const names = fileWithNames
-    .split('\n')
-    .slice(1)
-    .map((n) => simplePipe(n, extractName, normalizeName, mask))
-    .join('\r\n');
+  const isGoogleFormsCSV = pathToFileWithNames.endsWith('.csv');
+
+  const names = (
+    isGoogleFormsCSV
+      ? extractNamesFromGoogleForm(fileWithNames)
+      : extractNamesPlainList(fileWithNames)
+  ).join('\n');
 
   writeFile(pathToOutputFile, names);
 })(process.argv.splice(2));
